0accaa5978b690073b0a029f9e5f4571cc512a41
Tom Grennan <tgrennan@platinasystems.com>
2016-03-07 12:59:14 -0800

    update copyright
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- msg.go
+++ msg.go
@@ -1,4 +1,4 @@
-// Copyright 2015 Platina Systems, Inc. All rights reserved.
+// Copyright 2015-2016 Platina Systems, Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style license described in the
 // LICENSE file.
 
